5447d87db17367e69a8ae87a855261bc9e22ad10
CankivancYigit <CankivancYigit@github.com>
2021-01-24 01:01:59 +0300

    Car Movement And Finish Line Added

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    [SerializeField] float rotateAmount = 1f;
+    [SerializeField] float moveSpeed = 3f;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePos.x < 0)
+            {
+                TurnLeft();
+            }
+            else if (mousePos.x > 0)
+            {
+                TurnRight();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rgb.velocity = transform.up * moveSpeed;
+    }
+
+    void TurnLeft()
+    {
+        transform.Rotate(0, 0, rotateAmount);
+    }
+    void TurnRight()
+    {
+        transform.Rotate(0, 0, -rotateAmount);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            Debug.Log("abc");
+        }
+    }
+}
